06d77aa87e849b06657fd8e6bea8c6662fa084a0
kevin-chen <kc3585@nyu.edu>
2020-08-12 19:36:47 -0400

    push to amplify

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -5,7 +5,7 @@ import { Elements } from '@stripe/react-stripe-js';
 import CheckoutForm from "../components/CheckoutForm";
 
 const Checkout = () => {
-    const stripePromise = loadStripe('<stripe_public_key>');
+    const stripePromise = loadStripe('pk_test_9UyT4Sg5mj6ZX052Nffhxvqo00BkvywDH5');
 
     return (
         <section className="checkout-wrapper">
